cd5a31bfeb190f142fd27c1ef7e64031a78cfd42
Anvarbek <c1221sharp@gmail.com>
2025-02-28 18:07:29 +0500

    env changed

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import Search from "./Search";
 import WeatherDetails from "./WeatherDetails";
 
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState("Tashkent");
 
   const fetchWeatherApi = async (cityName) => {
-    const apiKey = import.meta.env.VITE_API_KEY;
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
 
     try {
